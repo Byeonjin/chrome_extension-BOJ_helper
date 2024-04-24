a0794ceb30cca810a9aaab443cd6115fcbab57f2
Byeonjin <quswlstkd88@naver.com>
2024-04-24 18:41:31 +0900

    [Refact] 토글 기능 코드 개선
    - getTagList 함수 제거
      listElement로 replaceTagListToToggleList 내부에서 다시 HTML 요소를 선택하기 때문에 제거 했습니다.

--- scripts/content.js
+++ scripts/content.js
@@ -1,28 +1,21 @@
 const SUMMARY_TITLE_COLSED = "▶️ [문제 분류 표시하기]";
 const SUMMARY_TITLE_OPENED = "▼ [문제 분류 가리기]";
 
-const getTagList = () => {
-  const problemTags = document.querySelector("#problem_tags .spoiler-list");
-
-  if (!problemTags) return;
-
-  return problemTags.innerHTML;
-};
-
 const replaceTagListToToggleList = () => {
   const problemTags = document.querySelector("#problem_tags");
-  const listElement = problemTags.querySelector(".spoiler-list");
-  const tagListElement = getTagList();
 
-  if (!tagListElement) {
+  if (!problemTags) {
     console.log(
-      "[BOG-helper]: 로그인 여부와 알고리즘 분류 표시 설정이 되어있는지 확인해주세요."
+      "[BOG-Otter-helper]: 로그인 여부와 알고리즘 분류 표시 설정이 되어있는지 확인해주세요."
     );
+    return;
   }
+
+  const listElement = problemTags.querySelector(".spoiler-list");
 
   const detailsElementHTML = `<details>
     <summary><h3 class="tag-visibility-toggle">${SUMMARY_TITLE_COLSED}<h3></summary>
-    ${tagListElement}
+    ${listElement.innerHTML}
   </details>`;
 
   listElement.remove();
